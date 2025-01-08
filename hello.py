90a1ee1cf533f0347a8c79d84b6506232726cc63
41vi4p <dav7.digi@yahoo.com>
2025-01-08 09:46:25 +0530

    notes

---
+++ hello.py
@@ -0,0 +1,22 @@
+print("Hello World!!")
+
+x= "Hello"
+y = "Hello"
+y =5
+print(x+str(y))
+
+print("\n")
+
+A= 5
+a= 4
+print(str(a)+str(A))
+print(a,A)
+
+print("\n")
+
+c= 25
+d=5j
+for i in range(5):
+    print(c+d)
+
+print(type(c+d))
