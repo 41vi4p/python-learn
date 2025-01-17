8c69e8332fd98ed3ebd6326a04ee93b879fdcf31
41vi4p <dav7.digi@yahoo.com>
2025-01-17 10:39:45 +0530

    notes15

--- dataplot.py
+++ dataplot.py
@@ -2,3 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 x = np.arange(0,100)
 print(x)
+
+y=2*x
+z=x**2
+fig = plt.figure()
+ax = fig.add_axes(x,y)
+ax.set_xlabel("x")
+ax.set_ylabel("2x")
+ax.set_title
